0381bca252b2033fb557882342ec0cf85a0c40de
Edward  Seymour <edwardseymour@Edwards-MacBook-Air.local>
2022-11-17 15:17:52 -0600

    Updated Predicting power python file

--- App/Predicting Power Using the NSRDB.py
+++ App/Predicting Power Using the NSRDB.py
@@ -39,7 +39,7 @@ x_train = pd.read_csv('../data/X_train2.csv')
 y_train = pd.read_csv('../data/Y_train2.csv')
 Electricity_cost = pd.read_csv('../data/StatesElectricity.csv')
 Attributes = pd.read_csv('../data/Attributes.csv')
-url1 = "https://api.geoapify.com/v1/geocode/search?text=" + Address + "%20United%20States&apiKey=" + str(API_KEY)
+url1 = "https://api.geoapify.com/v1/geocode/search?text=" + Address + "%20United%20States&apiKey=" + API_KEY
 
 y_train.drop(columns = ['Unnamed: 0'],inplace = True)
 x_train.drop(columns = ['Unnamed: 0'],inplace = True)
