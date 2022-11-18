2e43821dfc60955e7e06440fd20e418b1fbdab0d
Edward  Seymour <edwardseymour@Edwards-MacBook-Air.local>
2022-11-17 20:33:10 -0600

    Edited environments file for dependencies

--- App/Predicting_Power_Using_the_NSRDB.py
+++ App/Predicting_Power_Using_the_NSRDB.py
@@ -93,6 +93,7 @@ columns = ['Temperature', 'Pressure', 'Relative Humidity', 'Wind Speed','Dew Poi
 
 for col in columns:
     scale = float(Attributes.loc['scale_factor', col])
+    st.write(Locationdf[col].dtype)
     Locationdf[col] = Locationdf[col] / scale
 
 
