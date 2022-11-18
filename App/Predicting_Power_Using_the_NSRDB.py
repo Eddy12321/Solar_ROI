c94b48fdb466075b8e15bed147de139b73b3cc34
Edward  Seymour <edwardseymour@Edwards-MacBook-Air.local>
2022-11-17 18:53:43 -0600

    Edited environments file for dependencies

--- App/Predicting_Power_Using_the_NSRDB.py
+++ App/Predicting_Power_Using_the_NSRDB.py
@@ -6,11 +6,9 @@ import pandas as pd
 import numpy as np
 import os
 import pickle
-import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn import __version__ as sklearn_version
 from scipy.spatial import cKDTree
-import h5pyd
 import requests
 import json
 from requests.structures import CaseInsensitiveDict
@@ -28,34 +26,43 @@ def getLocationData(lat, lon, cols = ['meta']):
     return df
 
 
-API_KEY = st.secrets['GEOAPI_KEY']
+GEOAPI_KEY = st.secrets['GEOAPI_KEY']
+NSRDBAPI_KEY = st.secret['NSRDBAPI_KEY']
 Address = "1600%20Pennsylvania%20Avenue%20NW%2C%20Washington%2C%20DC%2020500%"
 InstallationCost = 2400
+year = 2020
+attributes = 'air_temperature,relative_humidity,dew_point,wind_speed,surface_pressure,total_precipitable_water,ghi'
+leap_year = 'false'
+interval = '30'
+utc = 'false'
+your_name = 'None+None'
+reason_for_use = 'beta+testing'
+your_affiliation = 'Springboard'
+your_email = 'edwardjs43@gmail.com'
+mailing_list = 'false'
 
-nsrdb = h5pyd.File("/nrel/nsrdb/v3/nsrdb_2020.h5", 'r')
 x_train = pd.read_csv('../data/X_train2.csv')
 y_train = pd.read_csv('../data/Y_train2.csv')
 Electricity_cost = pd.read_csv('../data/StatesElectricity.csv')
 Attributes = pd.read_csv('../data/Attributes.csv')
-url1 = "https://api.geoapify.com/v1/geocode/search?text=" + Address + "%20United%20States&apiKey=" + API_KEY
-
-y_train.drop(columns = ['Unnamed: 0'],inplace = True)
-x_train.drop(columns = ['Unnamed: 0'],inplace = True)
+url1 = "https://api.geoapify.com/v1/geocode/search?text=" + Address + "%20United%20States&apiKey=" + GEOAPI_KEY
 
 # Powered by Geoapify https://www.geoapify.com
-
 headers = CaseInsensitiveDict()
 headers['Accept'] = "application/json"
 resp1 = requests.get(url1, headers = headers)
-
-print(resp1.status_code)
-
 features1 = resp1.json()['features']
-
 properties1 = features1[1]['properties']
-lat1 = properties1['lat']
-lon1 = properties1['lon']
+lat = properties1['lat']
+lon = properties1['lon']
 state1 = properties1['state']
+
+url = 'https://developer.nrel.gov/api/nsrdb/v2/solar/psm3-download.csv?wkt=POINT({lon}%20{lat})&names={year}&leap_day={leap}&interval={interval}&utc={utc}&full_name={name}&email={email}&affiliation={affiliation}&mailing_list={mailing_list}&reason={reason}&api_key={api}&attributes={attr}'.format(year=year, lat=lat, lon=lon, leap=leap_year, interval=interval, utc=utc, name=your_name, email=your_email, mailing_list=mailing_list, affiliation=your_affiliation, reason=reason_for_use, api=NSRDBAPI_KEY, attr=attributes)
+
+Locationdf = pd.read_csv(url)
+
+y_train.drop(columns = ['Unnamed: 0'],inplace = True)
+x_train.drop(columns = ['Unnamed: 0'],inplace = True)
 
 expected_model_version = '1.0'
 model_path = '../models/power_predictor.pkl'
@@ -75,13 +82,12 @@ Electricity_cost.drop(columns = 'Unnamed: 0', inplace = True)
 Attributes.set_index('Unnamed: 0', inplace = True)
 
 columns = ['air_temperature', 'relative_humidity', 'dew_point', 'wind_speed', 'surface_pressure', 'total_precipitable_water', 'ghi']
-
+''''
 Locationdf1 = getLocationData(lat1, lon1, cols = columns)
-
 for col in columns:
     scale = float(Attributes.loc['scale_factor', col])
     Locationdf1[col] = Locationdf1[col] / scale
-
+'''
 column_mapper = {'air_temperature': 'TempOut',
                 'surface_pressure': 'Bar',
                 'relative_humidity': 'OutHum',
@@ -89,25 +95,25 @@ column_mapper = {'air_temperature': 'TempOut',
                 'dew_point': 'DewPt',
                 'ghi': 'SolarRad',
                 'total_precipitable_water': 'Rain'}
-Locationdf1.rename(columns = column_mapper, inplace=True)
+Locationdf.rename(columns = column_mapper, inplace=True)
 
-Locationdf1['Bar'] = Locationdf1['Bar']
-Locationdf1['Rain'] = Locationdf1['Rain'] / 0.25
-Locationdf1['SolarEnergy'] = Locationdf1['SolarRad'] * 0.5 / 11.622
-Locationdf1['Temp_Pressure_ratio'] = Locationdf1['TempOut'] / Locationdf1['Bar']
+Locationdf['Bar'] = Locationdf['Bar']
+Locationdf['Rain'] = Locationdf['Rain'] / 0.25
+Locationdf['SolarEnergy'] = Locationdf['SolarRad'] * 0.5 / 11.622
+Locationdf['Temp_Pressure_ratio'] = Locationdf['TempOut'] / Locationdf1['Bar']
 
-Locationdf1.replace(to_replace = {np.Inf: np.NAN}, inplace = True)
-Locationdf1.dropna(inplace = True)
+Locationdf.replace(to_replace = {np.Inf: np.NAN}, inplace = True)
+Locationdf.dropna(inplace = True)
 
 model.fit(x_train, y_train)
 
-Locationdf1['Power'] = model.predict(Locationdf1)
+Locationdf['Power'] = model.predict(Locationdf)
 
 StateElectricityCost1 = Electricity_cost.loc[state1]['Electricity Prices']
 
-Locationdf1['Money_saved'] = Locationdf1['Power'] * (StateElectricityCost1 / 2)
+Locationdf['Money_saved'] = Locationdf['Power'] * (StateElectricityCost1 / 2)
 
-dollars_saved1 = Locationdf1['Money_saved'].sum() / 100
+dollars_saved1 = Locationdf['Money_saved'].sum() / 100
 
 print('The amount that you would save per year at your location is $' + str(dollars_saved1))
 TimeTillPayed = InstallationCost / dollars_saved1
