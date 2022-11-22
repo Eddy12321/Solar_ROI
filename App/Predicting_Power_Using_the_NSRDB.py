cfbe84eda7f5bea306d8ff262d3674d9df670064
Edward  Seymour <edwardseymour@Edwards-MacBook-Air.local>
2022-11-22 17:03:49 -0600

    Update model path in python app file

--- App/Predicting_Power_Using_the_NSRDB.py
+++ App/Predicting_Power_Using_the_NSRDB.py
@@ -49,7 +49,7 @@ def main():
     resp1 = requests.get(url1, headers = headers)
     features1 = resp1.json()['features']
     st.write(features1)
-    properties1 = features1[1]['properties']
+    properties1 = features1[0]['properties']
     lat = properties1['lat']
     lon = properties1['lon']
     state1 = properties1['state']
