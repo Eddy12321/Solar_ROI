52b6c0d3a8532286418e5e837e8b66579ba1df4d
Edward  Seymour <edwardseymour@Edwards-MacBook-Air.local>
2022-11-22 16:44:15 -0600

    Update model path in python app file

--- App/Predicting_Power_Using_the_NSRDB.py
+++ App/Predicting_Power_Using_the_NSRDB.py
@@ -20,99 +20,118 @@ from requests.structures import CaseInsensitiveDict
 
 GEOAPI_KEY = st.secrets['GEOAPI_KEY']
 NSRDBAPI_KEY = "gIgh6128lU37WJnlZyJuSOHICrGTn1C59Z8tBnD8"
-Address = "1600%20Pennsylvania%20Avenue%20NW%2C%20Washington%2C%20DC%2020500"
-InstallationCost = 2400
-year = 2020
-attributes = 'air_temperature,relative_humidity,dew_point,wind_speed,surface_pressure,total_precipitable_water,ghi'
-leap_year = 'false'
-interval = '30'
-utc = 'false'
-your_name = 'None+None'
-reason_for_use = 'beta+testing'
-your_affiliation = 'Springboard'
-your_email = 'edwardjs43@gmail.com'
-mailing_list = 'false'
 
-x_train = pd.read_csv('./data/X_train2.csv')
-y_train = pd.read_csv('./data/Y_train2.csv')
-Electricity_cost = pd.read_csv('./data/StatesElectricity.csv')
-url1 = "https://api.geoapify.com/v1/geocode/search?text=" + Address + "%20United%20States&apiKey=" + GEOAPI_KEY
+st.title("Predicting Solar Power Using the NSRDB")
+st.markdown("This program will allow you to determine a timeline for paying off your solar installation. We begin by predicting the amount of solar power that can be generated at \
+     the entered location. We then use the expected installation cost and the average electricity cost in your region to let you know when you can expect to start turning a profit!")
 
-# Powered by Geoapify https://www.geoapify.com
-headers = CaseInsensitiveDict()
-headers['Accept'] = "application/json"
-resp1 = requests.get(url1, headers = headers)
-features1 = resp1.json()['features']
-properties1 = features1[1]['properties']
-lat = properties1['lat']
-lon = properties1['lon']
-state1 = properties1['state']
+def main():
+    year = 2020
+    attributes = 'air_temperature,relative_humidity,dew_point,wind_speed,surface_pressure,total_precipitable_water,ghi'
+    leap_year = 'false'
+    interval = '30'
+    utc = 'false'
+    your_name = 'None+None'
+    reason_for_use = 'beta+testing'
+    your_affiliation = 'Springboard'
+    your_email = 'edwardjs43@gmail.com'
+    mailing_list = 'false'
 
-url = 'https://developer.nrel.gov/api/nsrdb/v2/solar/psm3-download.csv?wkt=POINT({lon}%20{lat})&names={year}&leap_day={leap}&interval={interval}&utc={utc}&full_name={name}&email={email}&affiliation={affiliation}&mailing_list={mailing_list}&reason={reason}&api_key={api}&attributes={attr}'.format(year=year, lat=lat, lon=lon, leap=leap_year, interval=interval, utc=utc, name=your_name, email=your_email, mailing_list=mailing_list, affiliation=your_affiliation, reason=reason_for_use, api=NSRDBAPI_KEY, attr=attributes)
+    x_train = pd.read_csv('./data/X_train2.csv')
+    y_train = pd.read_csv('./data/Y_train2.csv')
+    Electricity_cost = pd.read_csv('./data/StatesElectricity.csv')
+    url1 = "https://api.geoapify.com/v1/geocode/search?text=" + Address + "%20United%20States&apiKey=" + GEOAPI_KEY
 
-Locationdf = pd.read_csv(url)
+    # Powered by Geoapify https://www.geoapify.com
+    headers = CaseInsensitiveDict()
+    headers['Accept'] = "application/json"
+    resp1 = requests.get(url1, headers = headers)
+    features1 = resp1.json()['features']
+    properties1 = features1[1]['properties']
+    lat = properties1['lat']
+    lon = properties1['lon']
+    state1 = properties1['state']
 
-Locationdf = Locationdf.iloc[:,:12]
-Locationdf.columns = Locationdf.loc[1]
-Locationdf.drop([0,1], inplace = True)
-Locationdf.drop(columns = ['Year', 'Month', 'Day', 'Hour', 'Minute'], inplace = True)
+    url = 'https://developer.nrel.gov/api/nsrdb/v2/solar/psm3-download.csv?wkt=POINT({lon}%20{lat})&names={year}&leap_day={leap}&interval={interval}&utc={utc}&full_name={name}&email={email}&affiliation={affiliation}&mailing_list={mailing_list}&reason={reason}&api_key={api}&attributes={attr}'.format(year=year, lat=lat, lon=lon, leap=leap_year, interval=interval, utc=utc, name=your_name, email=your_email, mailing_list=mailing_list, affiliation=your_affiliation, reason=reason_for_use, api=NSRDBAPI_KEY, attr=attributes)
 
-st.write(Locationdf.head())
+    Locationdf = pd.read_csv(url)
 
-y_train.drop(columns = ['Unnamed: 0'],inplace = True)
-x_train.drop(columns = ['Unnamed: 0'],inplace = True)
+    Locationdf = Locationdf.iloc[:,:12]
+    Locationdf.columns = Locationdf.loc[1]
+    Locationdf.drop([0,1], inplace = True)
+    Locationdf.drop(columns = ['Year', 'Month', 'Day', 'Hour', 'Minute'], inplace = True)
 
-''''
-expected_model_version = '1.0'
-model_path = './Models/power_predictor.pkl'
+    y_train.drop(columns = ['Unnamed: 0'], inplace = True)
+    x_train.drop(columns = ['Unnamed: 0'], inplace = True)
 
-if os.path.exists(model_path):
-    with open(model_path, 'rb') as f:
-        model = pickle.load(f)
-    if model.version != expected_model_version:
-        print("Expected model version doesn't match version loaded")
-    if model.sklearn_version != sklearn_version:
-        print("Warning: model created under different sklearn version")
-else:
-    print("Expected model not found")
-'''
+    model = make_pipeline(MinMaxScaler(), PCA(n_components = 9), GradientBoostingRegressor(learning_rate = 0.1, n_estimators = 77, max_depth = 6))
 
-model = make_pipeline(MinMaxScaler(), PCA(n_components = 9), GradientBoostingRegressor(learning_rate = 0.1, n_estimators = 77, max_depth = 6))
+    Electricity_cost.set_index('State', inplace = True)
+    Electricity_cost.drop(columns = 'Unnamed: 0', inplace = True)
 
-Electricity_cost.set_index('State', inplace = True)
-Electricity_cost.drop(columns = 'Unnamed: 0', inplace = True)
+    column_mapper2 = {'Temperature': 'TempOut',
+                    'Pressure': 'Bar',
+                    'Relative Humidity': 'OutHum',
+                    'Wind Speed': 'WindSpeed',
+                    'Dew Point': 'DewPt',
+                    'GHI': 'SolarRad',
+                    'Precipitable Water': 'Rain'}
+    Locationdf.rename(columns = column_mapper2, inplace=True)
 
-column_mapper2 = {'Temperature': 'TempOut',
-                'Pressure': 'Bar',
-                'Relative Humidity': 'OutHum',
-                'Wind Speed': 'WindSpeed',
-                'Dew Point': 'DewPt',
-                'GHI': 'SolarRad',
-                'Precipitable Water': 'Rain'}
-Locationdf.rename(columns = column_mapper2, inplace=True)
+    for col in Locationdf.columns:
+        Locationdf[col] = Locationdf[col].astype(float)
 
-for col in Locationdf.columns:
-    Locationdf[col] = Locationdf[col].astype(float)
+    Locationdf['Rain']  = Locationdf['Rain'] / 0.25
+    Locationdf['SolarEnergy'] = Locationdf['SolarRad'] * 0.5 / 11.622
+    Locationdf['Temp_Pressure_ratio'] = Locationdf['TempOut'] / Locationdf['Bar']
 
-Locationdf['Rain']  = Locationdf['Rain'] / 0.25
-Locationdf['SolarEnergy'] = Locationdf['SolarRad'] * 0.5 / 11.622
-Locationdf['Temp_Pressure_ratio'] = Locationdf['TempOut'] / Locationdf['Bar']
+    Locationdf.replace(to_replace = {np.Inf: np.NAN}, inplace = True)
+    Locationdf.dropna(inplace = True)
 
-Locationdf.replace(to_replace = {np.Inf: np.NAN}, inplace = True)
-Locationdf.dropna(inplace = True)
+    model.fit(x_train, y_train)
 
-st.write(Locationdf.head())
+    Locationdf['Power'] = model.predict(Locationdf)
+    Locationdf['Power'] = Locationdf['Power'] * (InstallationSize / 0.6)
 
-model.fit(x_train, y_train)
+    StateElectricityCost1 = Electricity_cost.loc[state1]['Electricity Prices']
 
-Locationdf['Power'] = model.predict(Locationdf)
+    Locationdf['Money_saved'] = Locationdf['Power'] * (StateElectricityCost1 / 2)
 
-StateElectricityCost1 = Electricity_cost.loc[state1]['Electricity Prices']
+    dollars_saved1 = Locationdf['Money_saved'].sum() / 100
 
-Locationdf['Money_saved'] = Locationdf['Power'] * (StateElectricityCost1 / 2)
+    st.write('The amount that you would save per year at your location is $' + str(dollars_saved1))
+    TimeTillPayed = InstallationCost / dollars_saved1
+    st.write('By the estimation, you would be aple to pay off your solar installation in ' + str(TimeTillPayed) + ' years!')
 
-dollars_saved1 = Locationdf['Money_saved'].sum() / 100
+def checkAddress():
+    if (Address):
+        Address = ""
+        tempAddress = Address.rsplit(" ")
+        for idx, word in enumerate(tempAddress):
+            if idx == 0:
+                Address += word
+            else:
+                if word == ',':
+                    Address +=  '%2C'
+                else:
+                    Address += '%20' + word
+        st.write(Address)
+    else:
+        st.markdown('Please enter a valid address.')
+    
+    runMain()
 
-st.write('The amount that you would save per year at your location is $' + str(dollars_saved1))
-TimeTillPayed = InstallationCost / dollars_saved1
-st.write('By the estimation, you would be aple to pay off your solar installation in ' + str(TimeTillPayed) + ' years!')
+def runMain():
+    if (Address and InstallationCost and InstallationSize):
+        main()
+    else:
+        return
+
+st.markdown('Please enter your address as it appears on google, except with a space on both sides of the commas. No need to enter the country.')
+Address = st.text_input('Form', value = "1600 Pennsylvania Avenue , Washington DC , 20500", on_change = checkAddress(), label_visibility = 'hidden')
+
+st.markdown('Please enter the expected cost of your solar panel installation in dollars.')
+InstallationCost = st.number_input('Form', value = 0.0, min_value = 1, label_visibility = 'hidden', on_change = runMain())
+
+st.markdown('Please enter the size of your installation in kW .')
+InstallationSize = st.number_input('Form', value = 0.0, min_value = 0.05, label_visibility = 'hidden', on_change = runMain())
